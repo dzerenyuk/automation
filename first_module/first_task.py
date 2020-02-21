674ec3ef9f088415455cd482533660a32f94f71b
dmytro.dzereniuk <dsdzerenyuk@gmail.com>
2020-02-21 17:19:13 +0200

    add edited tasks from module 1

---
+++ first_module/first_task.py
@@ -0,0 +1,6 @@
+# first task
+user_name = input('Please enter your name:')
+age = input('Please enter your age:')
+print(f'Hello {user_name} your age is {age}')
+
+
---
+++ first_module/third_task.py
@@ -0,0 +1,10 @@
+# third task
+first_number = int(input('Enter first number'))
+second_number = int(input('Enter second number'))
+print(first_number + second_number)  # addition
+print(first_number - second_number)  # substraction
+print(first_number * second_number)  # multiplication
+print(first_number // second_number)  # division with no remainder
+print(first_number / second_number)  # division with remainder
+print(first_number % second_number)  # remainder from division
+print(first_number ** second_number)  # powering
